375adacffffad99402112cc5c6705f3780d63173
natsuki <benja.natsu@gmail.com>
2018-09-22 00:03:48 -0300

    First model of a login page in xamarin

---
+++ XamarinApp/XamarinApp/XamarinApp/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using XamarinApp.Views;
+
+namespace XamarinApp
+{
+	public partial class App : Application
+	{
+        #region Constructor
+        public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new NavigationPage(new Login());
+		}
+        #endregion
+
+        #region Methods
+        protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+        #endregion
+    }
+}
