b5052b1c82a51469b4837abbc37cf4393943b598
natsuki <benja.natsu@gmail.com>
2018-09-22 01:20:59 -0300

    Validations and link View with ViewModel

--- XamarinApp/XamarinApp/XamarinApp/ViewModel/MainViewModel.cs
+++ XamarinApp/XamarinApp/XamarinApp/ViewModel/MainViewModel.cs
@@ -6,5 +6,16 @@ namespace XamarinApp.ViewModel
 {
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login { get; set; }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+
     }
 }
