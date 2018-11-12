ba7f328c733458cde729a92bdf4bd431668f7910
natsukis <benja_007_@hotmail.com>
2018-11-12 15:47:24 -0300

    Update the create view of users and now you can register users via backend

--- XamarinApp/XamarinApp.Backend/Controllers/UsersController.cs
+++ XamarinApp/XamarinApp.Backend/Controllers/UsersController.cs
@@ -9,6 +9,7 @@ using System.Web;
 using System.Web.Mvc;
 using XamarinApp.Backend.Models;
 using XamarinApp.Domain;
+using XamarinApp.Backend.Helpers;
 
 namespace XamarinApp.Backend.Controllers
 {
@@ -49,16 +50,31 @@ namespace XamarinApp.Backend.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "UserId,FirstName,LastName,Email,Telephone,ImagePath")] User user)
+        public async Task<ActionResult> Create( UserView view)
         {
             if (ModelState.IsValid)
             {
+                var user = this.ToUser(view);
                 db.Users.Add(user);
                 await db.SaveChangesAsync();
+                UsersHelper.CreateUserASP(view.Email, "User", view.Password);
                 return RedirectToAction("Index");
             }
 
-            return View(user);
+            return View(view);
+        }
+
+        private User ToUser(UserView view)
+        {
+            return new User
+            {
+                Email = view.Email,
+                FirstName = view.FirstName,
+                ImagePath = view.ImagePath,
+                LastName = view.LastName,
+                Telephone = view.Telephone,
+                UserId = view.UserId
+            };
         }
 
         // GET: Users/Edit/5
